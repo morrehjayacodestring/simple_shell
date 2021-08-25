7329219ac2978b60ef980e61bd490e68d1153af9
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-08-25 09:29:02 +0300

    Betty is proud

--- prompt.c
+++ prompt.c
@@ -103,7 +103,8 @@ write(STDOUT_FILENO, "$ ", 2);
 else
 non_interactive(env);
 signal(SIGINT, ctrl_c); /* makes ctrl+c not work */
-command = NULL; i = 0; /* reset vars each time loop runs */
+command = NULL;
+i = 0; /* reset vars each time loop runs */
 i = get_line(&command); /* read user's cmd in stdin */
 ctrl_D(i, command, env); /* exits shell if ctrl-D */
 n_command = command;
@@ -112,11 +113,13 @@ n = 0;
 while (command[n] != '\n') /* replace get_line's \n with \0 */
 n++;
 command[n] = '\0';
-if (command[0] == '\0') /* reprompt if user hits enter only */
+if (command[0] == '\0')   /* reprompt if user hits enter only */
 {
-free(n_command); continue;
+free(n_command);
 }
-token = NULL; token = _str_tok(command, " "); /*token user cmd*/
+continue;
+token = NULL;
+token = _str_tok(command, " "); /*token user cmd*/
 if (n_command != NULL)
 free(n_command);
 exit_stat = built_in(token, env, command_line_no, NULL);
